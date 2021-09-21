bf9b23ecacb8c98d28ad3333a98c9d315b685f07
vitorfn <vitor.nunes@estudante.ufla.br>
2021-09-21 17:25:18 -0300

    definição dos níveis de obesidade

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,30 @@ export class HomePage {
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    let classificacao = "";
+    let obesidadeGrau = 0;
+    if (imc < 18.5) {
+        classificacao = "Magreza";
+    } else if (imc > 18.5 && imc < 24.9) {
+        classificacao = "Normal";
+    } else if (imc > 25.0 && imc < 29.9) {
+        classificacao = "Sobrepeso";
+        obesidadeGrau = 1;
+    } else if (imc > 30.0 && imc < 39.9) {
+        classificacao = "Obesidade";
+        obesidadeGrau = 2;
+    } else if (imc > 40.0) {
+        classificacao = "Obesidade Grave";
+        obesidadeGrau = 3;
+    } 
+
+    if(obesidadeGrau > 0) {
+      this.showMessage(`IMC = ${imc.toFixed(2)} - Classificação = ${classificacao} - Grau de Obesidade = ${obesidadeGrau}`);
+    }
+
+    else {
+      this.showMessage(`IMC = ${imc.toFixed(2)} - Classificação = ${classificacao}`);
+    }
   }
 
   async showMessage(msg: string) {
